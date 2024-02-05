f8acea0c891242551faba126d9a9b3c4c96ed890
khpolash <khpolashbd@gmail.com>
2024-02-05 17:59:08 +0600

    Solve problem 61A Ultra-Fast Mathematician

---
+++ Ultra-FastMathematician/Program.cs
@@ -0,0 +1,11 @@
+﻿string input = Console.ReadLine()!;
+string input1 = Console.ReadLine()!;
+System.Text.StringBuilder sb = new System.Text.StringBuilder();
+for (int i = 0; i < input.Length; i++)
+{
+    int digit1 = input[i] - '0';
+    int digit2 = input1[i] - '0';
+    int answer = digit1 ^ digit2;
+    sb.Append(answer);
+}
+Console.WriteLine(sb.ToString());
