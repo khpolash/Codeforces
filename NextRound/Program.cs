e2ca528bd66335e4e327be9abd37df78d80f3f2d
khpolash <khpolashbd@gmail.com>
2024-02-05 16:41:11 +0600

    Solve problem 158A-Next Round

---
+++ NextRound/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] input = Console.ReadLine()!.Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+string[] input2 = Console.ReadLine()!.Split();
+
+int[] inputArray = new int[n];
+for (int i = 0; i < n; i++)
+{
+    inputArray[i] = int.Parse(input2[i]);
+}
+int participants = 0;
+
+for (int j = 0; j < n; j++)
+{
+    if (inputArray[j] > 0 && inputArray[j] >= inputArray[k - 1])
+    {
+        participants++;
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine(participants);
