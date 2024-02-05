c50bd65717d6a0214eda30c75cfa17cfcbcfe129
khpolash <khpolashbd@gmail.com>
2024-02-05 17:48:23 +0600

    Solve problem 271A Beautiful Year

---
+++ BeautifulYear/Program.cs
@@ -0,0 +1,15 @@
+﻿int year = int.Parse(Console.ReadLine()!);
+int nextYear = year + 1;
+while (!IsDistinct(nextYear))
+{
+    nextYear++;
+}
+
+Console.WriteLine(nextYear);
+
+static bool IsDistinct(int number)
+{
+    int[] digitArray = number.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+    HashSet<int> result = new(digitArray);
+    return (result.Count == digitArray.Length);
+}
