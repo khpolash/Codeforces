984eb82fe25a49d9c44f869b4eb31a34b8fe58fc
khpolash <khpolashbd@gmail.com>
2024-02-05 17:50:24 +0600

    Solve problem 1030A In Search of an Easy Problem

---
+++ InSearchOfAnEasyProblem/Program.cs
@@ -0,0 +1,4 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string[] options = Console.ReadLine()!.Split();
+bool isHard = Array.Exists(options, opinion => opinion == "1");
+Console.WriteLine(isHard ? "HARD" : "EASY");
