b9eba1842b710748d83064b4839e955e3ad5f74f
khpolash <khpolashbd@gmail.com>
2024-02-05 18:05:54 +0600

    Solve problem 1328A Divisibility Problem

---
+++ DivisibilityProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿int testCase = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < testCase; i++)
+{
+    string[] input = Console.ReadLine()!.Split();
+    int n = int.Parse(input[0]);
+    int m = int.Parse(input[1]);
+
+    int result = CalculateMoves(n, m);
+    Console.WriteLine(result);
+}
+
+static int CalculateMoves(int x, int y)
+{
+    return (x % y) == 0 ? 0 : (y - (x % y));
+}
