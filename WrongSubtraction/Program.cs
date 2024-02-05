a8966637511b35eb7744e6fc8dc639de76b9a184
khpolash <khpolashbd@gmail.com>
2024-02-05 17:29:34 +0600

    Solve problem 977A Wrong Subtraction

---
+++ WrongSubtraction/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] input = Console.ReadLine()!.Split();
+int n = int.Parse(input[0]);
+int k = int.Parse(input[1]);
+
+for (int i = 0; i < k; i++)
+{
+    if (n % 10 == 0)
+    {
+        n /= 10;
+    }
+    else
+    {
+        n--;
+    }
+}
+
+Console.WriteLine(n.ToString());
