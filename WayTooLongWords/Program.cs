163df71a5bdb8e0da9a1219bf608f22066ebc76d
khpolash <khpolashbd@gmail.com>
2024-02-05 16:33:36 +0600

    Solve Way Too Long Words

---
+++ WayTooLongWords/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    string inputWord = Console.ReadLine()!;
+    string result = AbbreviateIfTooLong(inputWord);
+    Console.WriteLine(result);
+}
+static string AbbreviateIfTooLong(string word)
+{
+    if (word.Length > 10)
+    {
+        int omittedLetters = word.Length - 2;
+        return $"{word[0]}{omittedLetters}{word[^1]}";
+    }
+    else
+    {
+        return word;
+    }
+}
