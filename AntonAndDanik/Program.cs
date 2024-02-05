eb4281e046773e91aff85baefcb92085da405c93
khpolash <khpolashbd@gmail.com>
2024-02-05 17:37:05 +0600

    Solve problem 734A Anton and Danik

---
+++ AntonAndDanik/Program.cs
@@ -0,0 +1,20 @@
+﻿int totalPlay = int.Parse(Console.ReadLine()!);
+
+string input = Console.ReadLine()!.ToUpper();
+
+int gameOwnA = 0;
+int gameOwnD = 0;
+
+for (int i = 0; i < totalPlay; i++)
+{
+    if (input[i] == 'A')
+    {
+        gameOwnA++;
+    }
+    else if (input[i] == 'D')
+    {
+        gameOwnD++;
+    }
+}
+
+Console.WriteLine((gameOwnA > gameOwnD) ? "Anton" : (gameOwnD > gameOwnA) ? "Danik" : "Friendship");
