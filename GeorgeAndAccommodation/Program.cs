7f412520190de730651017f5ed5d6f420fd22a6e
khpolash <khpolashbd@gmail.com>
2024-02-05 17:51:56 +0600

    Solve problem 467A George and Accommodation

---
+++ GeorgeAndAccommodation/Program.cs
@@ -0,0 +1,13 @@
+﻿int roomNumber = int.Parse(Console.ReadLine()!);
+
+int canMove = 0;
+
+for (int i = 0; i < roomNumber; i++)
+{
+    string[] input = Console.ReadLine()!.Split();
+    if (int.Parse(input[0]) + 2 <= int.Parse(input[1]))
+    {
+        canMove++;
+    }
+}
+Console.WriteLine(canMove.ToString());
