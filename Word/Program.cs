39bb4c84f656529a7e376c6a4a0af2a591ed9ac9
khpolash <khpolashbd@gmail.com>
2024-02-05 17:28:17 +0600

    solve problem 59A Word

---
+++ Word/Program.cs
@@ -0,0 +1,5 @@
+﻿string input = Console.ReadLine()!;
+
+int totalUpper = input.Count(c => char.IsUpper(c));
+int totalLower = input.Count(c => char.IsLower(c));
+Console.WriteLine(totalLower >= totalUpper ? input.ToLower() : input.ToUpper());
