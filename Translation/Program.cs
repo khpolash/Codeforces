98ed55085b0de6c223958d56b8a04d379d8084f0
khpolash <khpolashbd@gmail.com>
2024-02-05 17:39:27 +0600

    Solve problem 41A Translation

---
+++ Translation/Program.cs
@@ -0,0 +1,5 @@
+﻿string t = Console.ReadLine()!.Trim();
+string s = Console.ReadLine()!.Trim();
+string reversedString = new(s.Reverse().ToArray());
+
+Console.WriteLine((t.Length == reversedString.Length && t == reversedString) ? "YES" : "NO");
