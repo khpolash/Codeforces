5a6b25ed368722986162bffa85228cccee6ece8b
khpolash <khpolashbd@gmail.com>
2024-02-05 17:57:55 +0600

    Solve problem 200B Drinks

---
+++ Drinks/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string[] input = Console.ReadLine()!.Split();
+
+double sum = 0;
+
+for (int i = 0; i < n; i++)
+{
+    sum += int.Parse(input[i]);
+}
+Console.WriteLine(sum / n);
