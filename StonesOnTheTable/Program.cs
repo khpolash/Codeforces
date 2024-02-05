3d47391d55c7a7fe573355c27764be37bcad6296
khpolash <khpolashbd@gmail.com>
2024-02-05 17:20:37 +0600

    Solve problem 266A Stones on the Table

---
+++ StonesOnTheTable/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string input = Console.ReadLine()!;
+int minRemove = 0;
+for (int i = 1; i < n; i++)
+{
+    if (input[i] == input[i - 1])
+    {
+        minRemove++;
+    }
+}
+Console.WriteLine(minRemove);
