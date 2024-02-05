81128801e00c1c4205b8138635edfa10607b899c
khpolash <khpolashbd@gmail.com>
2024-02-05 18:04:40 +0600

    Solve problem 705A Hulk

---
+++ Hulk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+int n = int.Parse(Console.ReadLine()!);
+
+StringBuilder sb = new StringBuilder();
+
+for (int i = 1; i <= n; i++)
+{
+    sb.Append("I ").Append(i % 2 == 1 ? "hate" : "love");
+    if (i < n)
+        sb.Append(" that ");
+}
+sb.Append(" it");
+
+Console.WriteLine(sb.ToString());
