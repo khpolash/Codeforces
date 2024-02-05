30d28930e2ab22d7c0d07e7c1d1f62adebb54503
khpolash <khpolashbd@gmail.com>
2024-02-05 18:00:56 +0600

    Solve problem 228A Is your horseshoe on the other hoof

---
+++ IsYourHorseshoeOnTheOtherHoof/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] color = Console.ReadLine()!.Split();
+
+int color1 = int.Parse(color[0]);
+int color2 = int.Parse(color[1]);
+int color3 = int.Parse(color[2]);
+int color4 = int.Parse(color[3]);
+
+HashSet<int> uniqueColor = new HashSet<int>();
+uniqueColor.Add(color1);
+uniqueColor.Add(color2);
+uniqueColor.Add(color3);
+uniqueColor.Add(color4);
+
+Console.WriteLine(4 - uniqueColor.Count);
