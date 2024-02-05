ba15d96aecfe430b9620041affb19a2d34894f5b
khpolash <khpolashbd@gmail.com>
2024-02-05 17:24:44 +0600

    Solve problem 546A Soldier and Bananas

---
+++ SoldierAndBananas/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] inputs = Console.ReadLine()!.Split();
+int k = int.Parse(inputs[0]);
+int n = int.Parse(inputs[1]);
+int w = int.Parse(inputs[2]);
+int totalCost = ((w * (w + 1)) / 2) * k;
+int borrowAmount = Math.Max(0, totalCost - n);
+Console.WriteLine(borrowAmount.ToString());
