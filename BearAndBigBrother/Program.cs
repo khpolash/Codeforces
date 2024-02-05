36e80e2480b55f856682715d25c73f5d75db1def
khpolash <khpolashbd@gmail.com>
2024-02-05 17:21:56 +0600

    Solve problem 791A Bear and Big Brother

---
+++ BearAndBigBrother/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] input = Console.ReadLine()!.Split();
+
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+int years = 0;
+while (a <= b)
+{
+    a *= 3;
+    b *= 2;
+    years++;
+}
+
+Console.WriteLine(years);
