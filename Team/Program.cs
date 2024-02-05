8413f8cdbf7602554ade7f96947a49b8a35b9262
khpolash <khpolashbd@gmail.com>
2024-02-05 16:39:30 +0600

    Solve problem 231A-Team

---
+++ Team/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine()!.Split();
+    int sureCount = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        if (input[j] == "1")
+        {
+            sureCount++;
+        }
+    }
+    if (sureCount >= 2)
+    {
+        count++;
+    }
+}
