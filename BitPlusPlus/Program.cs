d442eef2d461990ea97046474277f95104fdec61
khpolash <khpolashbd@gmail.com>
2024-02-05 16:42:54 +0600

    Solve problem 282A-Bit++

---
+++ BitPlusPlus/Program.cs
@@ -0,0 +1,17 @@
+﻿int testCase = int.Parse(Console.ReadLine()!);
+
+int result = 0;
+
+for (int i = 0; i < testCase; i++)
+{
+    string input = Console.ReadLine()!;
+    if (input.Contains("++"))
+    {
+        result++;
+    }
+    else
+    {
+        result--;
+    }
+}
+Console.WriteLine(result);
