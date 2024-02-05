6a88076607b0940f92bd7654d909bf6f9574aebb
khpolash <khpolashbd@gmail.com>
2024-02-05 17:32:53 +0600

    Solve problem 110A Nearly Lucky Number

---
+++ NearlyLuckyNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿long n = long.Parse(Console.ReadLine()!);
+
+Console.WriteLine(IsNearlyLucky(n) ? "YES" : "NO");
+
+static bool IsNearlyLucky(long n)
+{
+    string numberString = n.ToString();
+    int luckyDigitsCount = 0;
+
+    foreach (char digit in numberString)
+    {
+        if (digit == '4' || digit == '7')
+        {
+            luckyDigitsCount++;
+        }
+    }
+
+    return IsLucky(luckyDigitsCount);
+}
+
+static bool IsLucky(int x)
+{
+    return x == 4 || x == 7 || x == 44 || x == 47 || x == 74 || x == 77;
+}
