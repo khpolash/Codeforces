44316f876e4dff8e82be1e55aac1ce191c6b5e56
khpolash <khpolashbd@gmail.com>
2024-02-05 17:55:18 +0600

    Solve problem 136A Presents

---
+++ Presents/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string[] input = Console.ReadLine()!.Split();
+int[] result = new int[n];
+for (int i = 0; i < n; i++)
+{
+    int giftGiver = int.Parse(input[i]);
+    result[giftGiver - 1] = i + 1;
+}
+
+Console.WriteLine(string.Join(" ", result));
