fca1678e19d855c7643a28d31d511f319e2ebe73
khpolash <khpolashbd@gmail.com>
2024-02-05 17:09:54 +0600

    Solve Problem 112A - Petya and Strings

---
+++ PetyaAndStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿string str1 = Console.ReadLine()!;
+string str2 = Console.ReadLine()!;
+
+int result = string.Compare(str1.ToLower(), str2.ToLower());
+
+if (result < 0)
+{
+    Console.WriteLine("-1");
+}
+else if (result > 0)
+{
+    Console.WriteLine("1");
+}
+else
+{
+    Console.WriteLine("0");
+}
