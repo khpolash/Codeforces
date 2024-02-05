aa98d2b49ea8beea07e131b16d3b03ebb3c3e275
khpolash <khpolashbd@gmail.com>
2024-02-05 17:53:44 +0600

    Solve problem 344A Magnets

---
+++ Magnets/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string[] positions = new string[n];
+for (int i = 0; i < positions.Length; i++)
+{
+    positions[i] = Console.ReadLine()!;
+}
+int groups = CountGroup(positions);
+Console.WriteLine(groups);
+static int CountGroup(string[] positions)
+{
+    int groups = 1;
+    for (int i = 1; i < positions.Length; i++)
+    {
+        if (positions[i] != positions[i - 1])
+        {
+            groups++;
+        }
+    }
+    return groups;
+}
