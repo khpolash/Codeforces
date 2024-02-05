3fef44a5c736a435b77e253cfa93fad3c69d29c9
khpolash <khpolashbd@gmail.com>
2024-02-05 17:43:29 +0600

    Solve problem 677A Vanya and Fence

---
+++ VanyaAndFence/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] input = Console.ReadLine()!.Split();
+string[] height = Console.ReadLine()!.Split();
+
+int n = int.Parse(input[0]);
+decimal h = decimal.Parse(input[1]);
+int result = 0;
+for (int i = 0; i < n; i++)
+{
+    result += (int)Math.Ceiling(int.Parse(height[i]) / h);
+}
+Console.WriteLine(result.ToString());
