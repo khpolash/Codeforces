46918e335f750183066b6b2016e96b73fbf16bef
khpolash <khpolashbd@gmail.com>
2024-02-05 17:41:17 +0600

    Solve problem 116A Tram

---
+++ Tram/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int currentCapacity = 0;
+int minCapacity = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine()!.Split();
+    int passengersExiting = int.Parse(input[0]);
+    int passengersEntering = int.Parse(input[1]);
+    currentCapacity = currentCapacity - passengersExiting + passengersEntering;
+    minCapacity = Math.Max(minCapacity, currentCapacity);
+}
+
+Console.WriteLine(minCapacity.ToString());
