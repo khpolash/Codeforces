7601bdc485f4ff1a27dda1d7f13316e42045a2a8
khpolash <khpolashbd@gmail.com>
2024-02-06 10:43:07 +0600

    Solve problem 155A I_love_\%username\%

---
+++ IloveUsername/Program.cs
@@ -0,0 +1,29 @@
+﻿int testCase = int.Parse(Console.ReadLine()!);
+
+string[] pointsString = Console.ReadLine()!.Split();
+
+int[] points = new int[testCase];
+
+for (int i = 0; i < testCase; i++)
+{
+    points[i] = int.Parse(pointsString[i]);
+}
+int bestPerformance = points[0];
+int worstPerformance = points[0];
+int amazingPerformances = 0;
+
+for (int i = 0; i < points.Length; i++)
+{
+    if (points[i] > bestPerformance)
+    {
+        bestPerformance = points[i];
+        amazingPerformances++;
+    }
+    else if (points[i] < worstPerformance)
+    {
+        worstPerformance = points[i];
+        amazingPerformances++;
+    }
+}
+
+Console.WriteLine(amazingPerformances);
