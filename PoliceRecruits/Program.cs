57abf64673d4c90ad7dcb42bfb30cd021a069e58
khpolash <khpolashbd@gmail.com>
2024-02-06 10:41:32 +0600

    Solve problem 427A Police Recruits

---
+++ PoliceRecruits/Program.cs
@@ -0,0 +1,27 @@
+﻿int testCase = int.Parse(Console.ReadLine()!);
+string[] events = Console.ReadLine()!.Split();
+int totalOfficers = 0;
+int untreatedCrimes = 0;
+
+for (int i = 0; i < testCase; i++)
+{
+    int eventValue = int.Parse(events[i]);
+    switch (eventValue)
+    {
+        case -1:
+            switch (totalOfficers)
+            {
+                case 0:
+                    untreatedCrimes++;
+                    break;
+                default:
+                    totalOfficers--;
+                    break;
+            }
+            break;
+        default:
+            totalOfficers += eventValue;
+            break;
+    }
+}
+Console.WriteLine(untreatedCrimes);
