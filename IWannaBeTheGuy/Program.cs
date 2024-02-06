99f9904e26c3f8a5b9b2cdda4e1c05206f347df4
khpolash <khpolashbd@gmail.com>
2024-02-06 10:14:01 +0600

    Solve problem 469A I Wanna Be the Guy

---
+++ IWannaBeTheGuy/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+
+int[] levelsX = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int[] levelsY = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+
+string result = CanPassWholeGame(n, levelsX, levelsY);
+Console.WriteLine(result);
+
+static string CanPassWholeGame(int n, int[] levelsX, int[] levelsY)
+{
+    HashSet<int> allLevels = new(Enumerable.Range(1, n));
+
+    HashSet<int> levelsXSet = new(levelsX.Skip(1));
+    HashSet<int> levelsYSet = new(levelsY.Skip(1));
+
+    HashSet<int> unionSet = new HashSet<int>(levelsXSet);
+    unionSet.UnionWith(levelsYSet);
+
+    return (unionSet.SetEquals(allLevels)) ? "I become the guy." : "Oh, my keyboard!";
+}
