ddb6fc818036a37c05a743a0a21cee09179b7f5c
khpolash <khpolashbd@gmail.com>
2024-02-06 10:29:51 +0600

    Solve problem 443A Anton and Letters

---
+++ AntonAndLetters/Program.cs
@@ -0,0 +1,11 @@
+﻿string input = Console.ReadLine()!;
+string lettersOnly = input[1..^1];
+
+Console.WriteLine(CountDistinct(lettersOnly));
+
+static int CountDistinct(string str)
+{
+    char[] digitArray = str.Where(ch => Char.IsLetter(ch)).ToArray();
+    HashSet<char> result = new(digitArray);
+    return (result.Count);
+}
