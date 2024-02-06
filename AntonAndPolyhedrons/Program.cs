3806f55bc02dbc9f97bddcdfec5e68ac28a6c7cb
khpolash <khpolashbd@gmail.com>
2024-02-06 10:32:41 +0600

    Solve problem 785A Anton and Polyhedrons

---
+++ AntonAndPolyhedrons/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine()!);
+int totalFace = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string polyhedron = Console.ReadLine()!;
+    if (Enum.TryParse(polyhedron, out PolyhedronType type))
+    {
+        totalFace += (int)type;
+    }
+}
+Console.WriteLine(totalFace);
+
+enum PolyhedronType
+{
+    Tetrahedron = 4,
+    Cube = 6,
+    Octahedron = 8,
+    Dodecahedron = 12,
+    Icosahedron = 20
+}
