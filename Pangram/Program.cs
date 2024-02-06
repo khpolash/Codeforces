f80624c5a3219cdf17bd378a02b36af5593d47f6
khpolash <khpolashbd@gmail.com>
2024-02-06 10:25:59 +0600

    Solve problem 520A Pangram

---
+++ Pangram/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+string input = Console.ReadLine()!;
+
+Console.WriteLine(IsPangram(input) ? "YES" : "NO");
+
+static bool IsPangram(string str)
+{
+    return str.ToLower().Where(c => char.IsLetter(c)).GroupBy(c => c).Count() == 26;
+}
