5d3063c07e3b8c7d91bd94f48ed7a54945516235
khpolash <khpolashbd@gmail.com>
2024-02-06 10:28:19 +0600

    Solve problem 148A Insomnia cure

---
+++ InsomniaCure/Program.cs
@@ -0,0 +1,18 @@
+﻿int k = int.Parse(Console.ReadLine()!);
+int l = int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+
+int d = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+
+for (int i = 1; i <= d; i++)
+{
+    if (i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
