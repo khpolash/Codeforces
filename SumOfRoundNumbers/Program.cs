abff79e97486520e6a6ddc313e83ad9e0d254d11
khpolash <khpolashbd@gmail.com>
2024-02-06 10:46:11 +0600

    Solve problem 1352A Sum of Round Numbers

---
+++ SumOfRoundNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    List<int> roundNumbers = SumOfRoundNumbers(n);
+
+    Console.WriteLine(roundNumbers.Count);
+    Console.WriteLine(string.Join(" ", roundNumbers));
+}
+
+static List<int> SumOfRoundNumbers(int n)
+{
+    List<int> result = new List<int>();
+    string nStr = n.ToString();
+
+    for (int i = 0; i < nStr.Length; i++)
+    {
+        int digit = int.Parse(nStr[i].ToString());
+        if (digit != 0)
+        {
+            result.Add(digit * (int)Math.Pow(10, nStr.Length - i - 1));
+        }
+    }
+    return result;
+}
