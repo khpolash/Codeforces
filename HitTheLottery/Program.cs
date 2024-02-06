e2ad5053dee1a656be707fcbad830039e43a4252
khpolash <khpolashbd@gmail.com>
2024-02-06 10:31:17 +0600

    Solve problem 996A Hit the Lottery

---
+++ HitTheLottery/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int n = int.Parse(Console.ReadLine()!);
+
+int bills100 = n / 100;
+int bills20 = (n % 100) / 20;
+int bills10 = ((n % 100) % 20) / 10;
+int bills5 = (((n % 100) % 20) % 10) / 5;
+int bills1 = (((n % 100) % 20) % 10) % 5;
+
+int totalBills = bills100 + bills20 + bills10 + bills5 + bills1;
+
+Console.WriteLine(totalBills);
