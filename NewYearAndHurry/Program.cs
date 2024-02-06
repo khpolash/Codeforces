596d1a09c4724161f95a0582686d517746ddd20e
khpolash <khpolashbd@gmail.com>
2024-02-06 11:26:55 +0600

    Solve problem 750A New Year and Hurry

---
+++ NewYearAndHurry/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] inputs = Console.ReadLine()!.Split();
+
+int n = int.Parse(inputs[0]);
+int k = int.Parse(inputs[1]);
+
+int totalMinutes = 240 - k;
+int solvedProblems = 0;
+int timeRequired = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    timeRequired += 5 * i;
+    if (timeRequired <= totalMinutes)
+    {
+        solvedProblems += 1;
+    }
+    else
+    {
+        break;
+    }
+}
+
+Console.WriteLine(solvedProblems);
