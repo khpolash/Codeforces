12f7b854281237471141d2a5e35203d59828c719
khpolash <khpolashbd@gmail.com>
2024-02-06 10:33:57 +0600

    Solve problem 268A Games

---
+++ Games/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] teams = new int[n, 2];
+for (int i = 0; i < n; i++)
+{
+    string[] colors = Console.ReadLine()!.Split();
+    teams[i, 0] = int.Parse(colors[0]);
+    teams[i, 1] = int.Parse(colors[1]);
+}
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i != j && teams[i, 0] == teams[j, 1])
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine(count);
+
