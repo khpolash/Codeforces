052175a3e12d264366a7fb9badfb8714b4cb42bb
khpolash <khpolashbd@gmail.com>
2024-02-06 14:40:17 +0600

    Solve problem 581A Vasya the Hipster

---
+++ VasyaTheHipster/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] inputs = Console.ReadLine()!.Split();
+
+int minSocks = Math.Min(int.Parse(inputs[0]), int.Parse(inputs[1]));
+int maxSocks = Math.Max(int.Parse(inputs[0]), int.Parse(inputs[1]));
+
+int dayDiffSocks = minSocks;
+int daySameSocks = (maxSocks - minSocks) / 2;
+
+Console.WriteLine($"{dayDiffSocks} {daySameSocks}");
