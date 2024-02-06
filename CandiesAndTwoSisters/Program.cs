be96a8c0f99f491449b80c1a3b2ee19bce35f292
khpolash <khpolashbd@gmail.com>
2024-02-06 10:36:25 +0600

    Solve problem 1335A Candies and Two Sisters

---
+++ CandiesAndTwoSisters/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int x = int.Parse(Console.ReadLine()!);
+    Console.WriteLine((x % 2 != 0) ? ((x - 1) / 2) : ((x / 2) - 1));
+}
