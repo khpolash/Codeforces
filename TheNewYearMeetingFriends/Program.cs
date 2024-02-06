a1e0b61e8974c5e7f6cde59c1760d83e0c52d4bb
khpolash <khpolashbd@gmail.com>
2024-02-06 13:11:54 +0600

    Solve problem 723A The New Year: Meeting Friends

---
+++ TheNewYearMeetingFriends/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] inputs = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+Array.Sort(inputs);
+
+int medium = inputs[1];
+
+int minimumDistance = Math.Abs(inputs[0] - medium) + Math.Abs(inputs[1] - medium) + Math.Abs(inputs[2] - medium);
+
+Console.WriteLine(minimumDistance);
