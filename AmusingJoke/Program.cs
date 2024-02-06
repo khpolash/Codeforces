a394691b98eb59e894d53c73916a56b6de69377c
khpolash <khpolashbd@gmail.com>
2024-02-06 10:37:36 +0600

    Solve problem 141A Amusing Joke

---
+++ AmusingJoke/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string guestName = Console.ReadLine()!;
+string hostName = Console.ReadLine()!;
+string pileOfLetters = Console.ReadLine()!;
+
+string combinedNames = $"{guestName}{hostName}";
+
+Console.WriteLine(CanPermuteNames(combinedNames, pileOfLetters) ? "YES" : "NO");
+
+static bool CanPermuteNames(string combinedNames, string pileOfLetters)
+{
+    if (combinedNames.Length != pileOfLetters.Length)
+    {
+        return false;
+    }
+    char[] combinedNamesArray = combinedNames.ToCharArray();
+    char[] pileOfLettersArray = pileOfLetters.ToCharArray();
+    Array.Sort(combinedNamesArray);
+    Array.Sort(pileOfLettersArray);
+
+    return combinedNamesArray.SequenceEqual(pileOfLettersArray);
+}
