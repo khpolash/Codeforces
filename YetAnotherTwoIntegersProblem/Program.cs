a0c176dc4082e8be74eee17a992b05800dccb8e6
khpolash <khpolashbd@gmail.com>
2024-02-08 00:49:29 +0600

    Solve problem 1409A Yet Another Two Integers Problem

---
+++ YetAnotherTwoIntegersProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+
+    int diff = Math.Abs(inputs[0] - inputs[1]);
+    int moves = (diff / 10) + (diff % 10 == 0 ? 0 : 1);
+    Console.WriteLine(moves);
+}
+
