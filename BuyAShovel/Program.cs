03e35b46dd8c1fdae2c1333d546e0d4be999477b
khpolash <khpolashbd@gmail.com>
2024-02-07 16:20:48 +0600

    Solve problem 732A Buy a Shovel

---
+++ BuyAShovel/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] inputs = Console.ReadLine()!.Split();
+
+int k = int.Parse(inputs[0]);
+int r = int.Parse(inputs[1]);
+
+int minNumberOfShovel = 1;
+
+while ((k * minNumberOfShovel) % 10 != 0 && (k * minNumberOfShovel) % 10 != r)
+{
+    minNumberOfShovel++;
+}
+
+Console.WriteLine(minNumberOfShovel);
