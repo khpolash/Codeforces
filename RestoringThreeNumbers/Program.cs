f00d9ce523f54da1aba5ccdc46e1651a0651e267
khpolash <khpolashbd@gmail.com>
2024-02-07 16:21:12 +0600

    Solve problem 1154 Restoring Three Numbers

---
+++ RestoringThreeNumbers/Program.cs
@@ -0,0 +1,3 @@
+﻿int[] inputs = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Array.Sort(inputs);
+Console.WriteLine($"{inputs[3] - inputs[1]} {inputs[3] - inputs[2]} {inputs[3] - inputs[0]}");
