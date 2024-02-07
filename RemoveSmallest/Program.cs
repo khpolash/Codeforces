12e24518aefaacf254aa1300dc6a4a721c4617a3
khpolash <khpolashbd@gmail.com>
2024-02-08 00:29:45 +0600

    Solve problem 1399A Remove Smallest

---
+++ RemoveSmallest/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int testCase = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < testCase; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] inputs = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+
+    string result = IsPossibleToOneElement(n, inputs) ? "YES" : "NO";
+    Console.WriteLine(result);
+}
+
+static bool IsPossibleToOneElement(int n, int[] a)
+{
+    bool flag = true;
+    int[] distinctElements = a.Distinct().ToArray();
+    Array.Sort(distinctElements);
+
+    for (int i = 1; i < distinctElements.Length; i++)
+    {
+        if (distinctElements[i] - distinctElements[i - 1] > 1)
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    return flag;
+}
