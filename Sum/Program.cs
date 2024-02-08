c06ec4bf2007b68c7ca8cfaccaa96fa99b0b5ad3
khpolash <khpolashbd@gmail.com>
2024-02-08 10:16:20 +0600

    Solve problem 1742A Sum

---
+++ Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine("Codeforce Problem NO: 1742A Sum");
+Console.WriteLine("==================================\t");
+
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    int[] numbers = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse).ToArray();
+    Array.Sort(numbers);
+
+    if (numbers[0] + numbers[0 + 1] == numbers[2])
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+
+}
+
