765fb52308bc92149a1498e5d5af0dcced1422f6
khpolash <khpolashbd@gmail.com>
2024-02-10 10:23:58 +0600

    Solve problem 1703A YES or YES?

---
+++ YESORYES/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1703A YES or YES?");
+Console.WriteLine("===================");
+
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    string inputs = Console.ReadLine()!.ToLower();
+    Console.WriteLine((inputs == "yes") ? "YES" : "NO");
+}
