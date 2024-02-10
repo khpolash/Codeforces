3b3c25d6166c44050316560a2a3b24fede0665c6
khpolash <khpolashbd@gmail.com>
2024-02-10 12:23:46 +0600

    Solve problem 381A Sereja and Dima

---
+++ SerejaAndDima/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("381A\tSereja and Dima");
+Console.WriteLine("=========================\n");
+
+int n = int.Parse(Console.ReadLine()!);
+
+int[] cards = new int[n];
+
+string[] inputs = Console.ReadLine()!.Split();
+
+int SerejaTotalCard = 0;
+int DimaTotalCard = 0;
+
+int turn = 0;
+
+for (int i = 0; i < cards.Length; i++)
+{
+    cards[i] = int.Parse(inputs[i]);
+}
+
+while (cards.Length > 0)
+{
+    if (turn % 2 == 0)
+    {
+        if (cards[0] > cards[^1])
+        {
+            SerejaTotalCard += cards[0];
+            cards = cards.Skip(1).ToArray();
+        }
+        else
+        {
+            SerejaTotalCard += cards[^1];
+            cards = cards.SkipLast(1).ToArray();
+        }
+        turn++;
+    }
+    else
+    {
+        if (cards[0] > cards[^1])
+        {
+            DimaTotalCard += cards[0];
+            cards = cards.Skip(1).ToArray();
+        }
+        else
+        {
+            DimaTotalCard += cards[^1];
+            cards = cards.SkipLast(1).ToArray();
+        }
+        turn++;
+    }
+
+}
+
+Console.WriteLine($"{SerejaTotalCard} {DimaTotalCard}");
