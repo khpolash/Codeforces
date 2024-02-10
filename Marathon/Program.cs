1928747ec6be2d4dfc9910bf4f52ed64f2c9db98
khpolash <khpolashbd@gmail.com>
2024-02-10 23:52:18 +0600

    Solve problem 1692A Marathon

---
+++ Marathon/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1692A Marathon");
+Console.WriteLine("===============\n");
+
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    int count = 0;
+
+    if (inputs[1] > inputs[0])
+        count++;
+    if (inputs[2] > inputs[0])
+        count++;
+    if (inputs[3] > inputs[0])
+        count++;
+
+    Console.WriteLine(count);
+}
