9a5aeac34d8ce77474d99ad33196b39d208fab5e
khpolash <khpolashbd@gmail.com>
2024-02-12 00:17:30 +0600

    Solve problem 1669A Division

---
+++ Division/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1669A\tDivision");
+Console.WriteLine("===============\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int rating = int.Parse(Console.ReadLine()!);
+
+    switch (rating)
+    {
+        case >= 1900:
+            Console.WriteLine($"Division 1");
+            break;
+        case >= 1600 and <= 1899:
+            Console.WriteLine($"Division 2");
+            break;
+        case >= 1400 and <= 1599:
+            Console.WriteLine($"Division 3");
+            break;
+        default:
+            Console.WriteLine($"Division 4");
+            break;
+    }
+}
