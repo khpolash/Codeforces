96e58aad741584a34a963f5dcdfbc26e97f2a032
khpolash <khpolashbd@gmail.com>
2024-02-11 23:08:20 +0600

    Solve problem 1367A Short Substrings

---
+++ ShortSubstrings/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("1367A\tShort Substrings");
+Console.WriteLine("==========================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    string b = Console.ReadLine()!;
+    string result = GuessStringA(b);
+    Console.WriteLine(result);
+}
+
+static string GuessStringA(string b)
+{
+    int n = b.Length;
+    string a = b[..1];
+
+    for (int i = 1; i < n; i += 2)
+    {
+        a += b[i];
+    }
+
+    return a;
+}
