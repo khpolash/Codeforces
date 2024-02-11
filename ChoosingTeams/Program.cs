9ebaf33b5f5ba45c59b0e381c58591eaed133820
khpolash <khpolashbd@gmail.com>
2024-02-11 16:36:29 +0600

    Solve problem 1676A  Lucky?

---
+++ ChoosingTeams/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("432A\tChoosing Teams");
+Console.WriteLine("=========================\n");
+
+string[] inputs = Console.ReadLine()!.Split();
+
+int n = int.Parse(inputs[0]);
+int k = int.Parse(inputs[1]);
+int teams = 0;
+int[] participant = new int[n];
+string[] strings = Console.ReadLine()!.Split();
+for (int i = 0; i < n; i++)
+{
+
+    participant[i] = int.Parse(strings[i]);
+}
+
+for (int i = 0; i < n; i++)
+{
+    if (5 - participant[i] >= k)
+    {
+        teams++;
+    }
+}
+
+Console.WriteLine(teams / 3);
+
---
+++ Lucky/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1676A\tLucky?");
+Console.WriteLine("=================\n");
+
+int n = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < n; i++)
+{
+    string inputs = Console.ReadLine()!;
+
+    string first = inputs[..^(inputs.Length / 2)];
+    string second = inputs[(inputs.Length / 2)..];
+
+    int sum1 = first.Sum(x => x);
+    int sum2 = second.Sum(x => x);
+
+    Console.WriteLine(sum1 == sum2 ? "YES" : "NO");
+}
