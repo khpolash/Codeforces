7c66edd3684fa713b871f04b174c83a7c45b7e67
khpolash <khpolashbd@gmail.com>
2024-02-12 23:58:57 +0600

    Solve problem 1791A Codeforces Checking

---
+++ CodeforcesChecking/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1791A Codeforces Checking!");
+Console.WriteLine("==============================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+string str = "codeforces";
+
+for (int i = 0; i < t; i++)
+{
+    char a = char.Parse(Console.ReadLine()!.ToLower());
+    Console.WriteLine(str.Contains(a) ? "YES" : "NO");
+}
+
