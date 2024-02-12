a14faaa0a372659560d05fe9957138da866e5d0d
khpolash <khpolashbd@gmail.com>
2024-02-13 00:29:39 +0600

    Solve problem 1760A Medium Number!

---
+++ MediumNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1760A\tMedium Number!");
+Console.WriteLine("=========================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    Array.Sort(inputs);
+
+    Console.WriteLine(inputs[1]);
+}
