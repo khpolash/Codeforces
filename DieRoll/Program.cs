6a4db2c71294bd89966ddd176b46d81f242ed208
khpolash <khpolashbd@gmail.com>
2024-02-13 00:15:06 +0600

    Solve problem 9A Die Roll

---
+++ DieRoll/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("9A\tDie Roll!");
+Console.WriteLine("=======================\n");
+
+int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int maxPoint = Math.Max(inputs[0], inputs[1]);
+int winningPossibilities = 6 - maxPoint + 1;
+int gcd = GCD(winningPossibilities, 6);
+Console.WriteLine($"{winningPossibilities / gcd}/{6 / gcd}");
+
+static int GCD(int a, int b)
+{
+    return b == 0 ? a : GCD(b, a % b);
+}
