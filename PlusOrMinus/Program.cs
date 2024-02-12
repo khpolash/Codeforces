23e2e5b6f9d4a79505a46df95a6edcca2e916cfe
khpolash <khpolashbd@gmail.com>
2024-02-12 22:42:54 +0600

    Solve problem  1807A Plus or Minus!

---
+++ PlusOrMinus/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1807A Plus or Minus!");
+Console.WriteLine("========================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] numbers = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    if (numbers[0] + numbers[1] == numbers[2])
+    {
+        Console.WriteLine("+");
+    }
+    else if (numbers[0] - numbers[1] == numbers[2])
+    {
+        Console.WriteLine("-");
+    }
+}
