cc7c6d6ba02840423c9a11f858c2a9a1b2ff2460
khpolash <khpolashbd@gmail.com>
2024-02-12 22:19:03 +0600

    Solve problem 703A Mishka and Game

---
+++ MishkaAndGame/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("703A\tMishka and Game!");
+Console.WriteLine("===========================\n");
+
+int n = int.Parse(Console.ReadLine()!);
+
+int mishkaWin = 0;
+int chrisWin = 0;
+for (int i = 0; i < n; i++)
+{
+    int[] ints = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    if (ints[0] > ints[1])
+    {
+        mishkaWin++;
+    }
+    else if (ints[0] < ints[1])
+    {
+        chrisWin++;
+    }
+
+}
+
+Console.WriteLine(mishkaWin > chrisWin ? "Mishka" : chrisWin > mishkaWin ? "Chris" : "Friendship is magic!^^");
