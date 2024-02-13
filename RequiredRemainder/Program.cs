5fb0e14e77d912557c0ee93a75c8bc2a7c7691a3
khpolash <khpolashbd@gmail.com>
2024-02-13 10:04:51 +0600

    Solve problem 1374A Required Remainder

---
+++ RequiredRemainder/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1374A\tRequired Remainder!");
+Console.WriteLine("=============================\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    string[] inputs = Console.ReadLine()!.Split();
+    long x = int.Parse(inputs[0]);
+    long y = int.Parse(inputs[1]);
+    long n = int.Parse(inputs[2]);
+
+    double k = n - (n % x) + y;
+
+    Console.WriteLine(k > n ? (n - n % x - (x - y)) : k);
+}
+
+
+
