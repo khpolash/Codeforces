a00b3b5c6a6284791559f1474b4f6aa16f3a0818
khpolash <khpolashbd@gmail.com>
2024-02-13 23:29:04 +0600

    Solve problem 1624A Plus One on the Subset

---
+++ PlusOneOnTheSubset/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1624A\tPlus One on the Subset!");
+Console.WriteLine("=================================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] arr = new int[n];
+    string[] str = Console.ReadLine()!.Split();
+    for (int j = 0; j < n; j++)
+    {
+        arr[j] = int.Parse(str[j]);
+    }
+
+    int minValue = arr.Min();
+    int maxValue = arr.Max();
+    Console.WriteLine(maxValue - minValue);
+}
