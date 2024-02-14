21ec15db19ad587cc4accbc4d76b4066091f849b
khpolash <khpolashbd@gmail.com>
2024-02-14 14:43:06 +0600

    Solve problem 8-0A Panoramix's Prediction

---
+++ Panoramix'sPrediction/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("8-0A Panoramix's Prediction!");
+Console.WriteLine("=================================\n");
+
+int[] input = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int firstPrime = input[0];
+int nextPrime = FindNextPrime(firstPrime);
+Console.WriteLine(nextPrime == input[1] ? "YES" : "NO");
+
+static int FindNextPrime(int start)
+{
+    int next = start + 1;
+    while (true)
+    {
+        if (IsPrime(next))
+            return next;
+
+        next++;
+    }
+}
+
+static bool IsPrime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
