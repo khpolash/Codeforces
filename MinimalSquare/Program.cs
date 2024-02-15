e405bdf6c79b03d64b97f9f9081ebfd6803aacdc
khpolash <khpolashbd@gmail.com>
2024-02-15 15:23:46 +0600

    Solve problem 1360A Minimal Square

---
+++ MinimalSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1360A Minimal Square!");
+Console.WriteLine("=============================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int a = inputs[0];
+    int b = inputs[1];
+
+    int result = (Math.Min(Math.Max(2 * b, a), Math.Max(2 * a, b)));
+
+    Console.WriteLine(Math.Pow(result, 2));
+}
