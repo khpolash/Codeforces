145e9155a27e9e08309740c5f5c80e0546c7d296
khpolash <khpolashbd@gmail.com>
2024-02-15 10:06:03 +0600

    solve problem 1551A Polycarp and Coins

---
+++ PolycarpAndCoins/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1551A Polycarp and Coins!");
+Console.WriteLine("===============================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+
+    int c2 = n / 3;
+    int c1 = n - c2 * 2;
+
+    Console.WriteLine($"{c1} {c2}");
+}
