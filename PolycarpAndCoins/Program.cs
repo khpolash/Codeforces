018d12d01f56e78099d7345ebb0e9b6061aa1521
khpolash <khpolashbd@gmail.com>
2024-02-15 11:03:16 +0600

    Revert "solve issue"
    This reverts commit 149f1a3e85a9e1af4c0bd13efd222553b48ec39d.

--- PolycarpAndCoins/Program.cs
+++ PolycarpAndCoins/Program.cs
@@ -6,19 +6,10 @@ int t = int.Parse(Console.ReadLine()!);
 
 for (int i = 0; i < t; i++)
 {
-    double n = double.Parse(Console.ReadLine()!);
+    int n = int.Parse(Console.ReadLine()!);
 
-    double c1 = Math.Ceiling(n / 3);
-    double c2 = Math.Floor((n - c1) / 2);
+    int c2 = n / 3;
+    int c1 = n - c2 * 2;
 
-    if (c1 + 2 * c2 == n)
-    {
-        Console.WriteLine($"{c1} {c2}");
-    }
-    else
-    {
-        Console.WriteLine($"{c2} {c1}");
-    }
-
-
+    Console.WriteLine($"{c1} {c2}");
 }
