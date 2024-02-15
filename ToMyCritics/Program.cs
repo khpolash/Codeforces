553e7445871ce535091b6234892109e3de7d6ddb
khpolash <khpolashbd@gmail.com>
2024-02-15 17:52:38 +0600

    Solve problem 1850A To My Critics!

---
+++ ToMyCritics/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1850A To My Critics!");
+Console.WriteLine("=======================\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    if (inputs[0] + inputs[1] >= 10 || inputs[0] + inputs[2] >= 10 || inputs[1] + inputs[2] >= 10)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+
+}
