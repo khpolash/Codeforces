8daaf1ee3a60ffcf8d53bc8f836ab65e903ff8a4
khpolash <khpolashbd@gmail.com>
2024-02-17 00:46:15 +0600

    Solve problem 1280A Minutes Before the New Year!

---
+++ MinutesBeforeTheNewYear/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1280A Minutes Before the New Year!\n");
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+    int remainingMinutes = (24 * 60) - ((inputs[0] * 60) + inputs[1]);
+
+    Console.WriteLine(remainingMinutes);
+}
