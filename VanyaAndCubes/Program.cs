de20c5374cd7d60d04d11c18829f40a19d81fa00
khpolash <khpolashbd@gmail.com>
2024-02-16 23:33:57 +0600

    solve problem 492A Vanya and Cubes

---
+++ VanyaAndCubes/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("492A Vanya and Cubes!");
+Console.WriteLine("==========================\n");
+
+int cubes = int.Parse(Console.ReadLine()!);
+
+int i = 1; int m = 1; int height = 0;
+
+while (m <= cubes)
+{
+    m = ((i * i) + i) / 2;
+    if (m > cubes)
+    {
+        break;
+    }
+    cubes -= m;
+    height++;
+    i++;
+}
+Console.WriteLine(height);
