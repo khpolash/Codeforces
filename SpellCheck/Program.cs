3d3b3fdc417298a58e8c3ab2294595fe2eb1bb27
khpolash <khpolashbd@gmail.com>
2024-02-17 15:53:37 +0600

    Solve problem 1722A Spell Check!

---
+++ SpellCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1722A Spell Check!\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+char[] target = "Timur".ToCharArray();
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    string input = Console.ReadLine()!;
+    char[] nameArray = new char[n];
+    for (int j = 0; j < n; j++)
+    {
+        nameArray[j] = input[j];
+    }
+    Array.Sort(target);
+    Array.Sort(nameArray);
+    bool result = target.SequenceEqual(nameArray);
+    Console.WriteLine(result ? "YES" : "NO");
+}
