d939f1d64335f7042cc7fe251d3199d6b94f8c28
khpolash <khpolashbd@gmail.com>
2024-02-17 12:41:50 +0600

    Solve problem 707A Brain's Photos!

---
+++ Brain'sPhotos/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("707A Brain's Photos!\n");
+
+string[] n = Console.ReadLine()!.Split();
+
+int rows = int.Parse(n[0]);
+int cols = int.Parse(n[1]);
+
+char[,] array = new char[rows, cols];
+
+for (int i = 0; i < rows; i++)
+{
+    string[] inputs = Console.ReadLine()!.ToUpper().Split();
+    for (int j = 0; j < cols; j++)
+    {
+        array[i, j] = char.Parse(inputs[j]);
+    }
+}
+
+bool result = array.Cast<char>().All(c => c == 'W' || c == 'B' || c == 'G');
+
+Console.WriteLine(result ? "#Black&White" : "#Color");
+
--- IsYourHorseshoeOnTheOtherHoof/Program.cs
+++ IsYourHorseshoeOnTheOtherHoof/Program.cs
@@ -1,14 +1,20 @@
-﻿string[] color = Console.ReadLine()!.Split();
+﻿//string[] color = Console.ReadLine()!.Split();
 
-int color1 = int.Parse(color[0]);
-int color2 = int.Parse(color[1]);
-int color3 = int.Parse(color[2]);
-int color4 = int.Parse(color[3]);
+//int color1 = int.Parse(color[0]);
+//int color2 = int.Parse(color[1]);
+//int color3 = int.Parse(color[2]);
+//int color4 = int.Parse(color[3]);
 
-HashSet<int> uniqueColor = new HashSet<int>();
-uniqueColor.Add(color1);
-uniqueColor.Add(color2);
-uniqueColor.Add(color3);
-uniqueColor.Add(color4);
+//HashSet<int> uniqueColor = new HashSet<int>();
+//uniqueColor.Add(color1);
+//uniqueColor.Add(color2);
+//uniqueColor.Add(color3);
+//uniqueColor.Add(color4);
 
-Console.WriteLine(4 - uniqueColor.Count);
+//Console.WriteLine(4 - uniqueColor.Count);
+
+int[] color = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int x = color.Distinct().Count();
+
+Console.WriteLine(color.Length - x);
