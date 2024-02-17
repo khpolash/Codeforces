3fb67249495cc8177f2f106e41b83cfe8e6046d7
khpolash <khpolashbd@gmail.com>
2024-02-17 15:27:23 +0600

    Solve problem 1873A Short Sort!

--- IWannaBeTheGuy/Program.cs
+++ IWannaBeTheGuy/Program.cs
@@ -1,4 +1,6 @@
-﻿int n = int.Parse(Console.ReadLine()!);
+﻿Console.WriteLine("469A  I Wanna Be The Guy!\n");
+
+int n = int.Parse(Console.ReadLine()!);
 
 int[] levelsX = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
 int[] levelsY = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
@@ -14,8 +16,13 @@ static string CanPassWholeGame(int n, int[] levelsX, int[] levelsY)
     HashSet<int> levelsXSet = new(levelsX.Skip(1));
     HashSet<int> levelsYSet = new(levelsY.Skip(1));
 
+    Console.WriteLine(string.Join(" ", levelsXSet));
+
     HashSet<int> unionSet = new HashSet<int>(levelsXSet);
     unionSet.UnionWith(levelsYSet);
+
+    Console.WriteLine(unionSet.Count);
+    Console.WriteLine(string.Join(" ", unionSet));
 
     return (unionSet.SetEquals(allLevels)) ? "I become the guy." : "Oh, my keyboard!";
 }
---
+++ ShortSort/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("1873A Short Sort!\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    string cards = Console.ReadLine()!;
+    string target = new(cards.OrderBy(x => x).ToArray());
+    int wrongPosition = 0;
+
+    for (int j = 0; j < cards.Length; j++)
+    {
+        if (target[j] != cards[j])
+        {
+            wrongPosition++;
+        }
+    }
+
+    Console.WriteLine(wrongPosition > 2 ? "NO" : "YES");
+
+}
