7ee647d4a8e114cfbe9338b1badac1d98ced19cf
khpolash <khpolashbd@gmail.com>
2024-02-17 11:17:57 +0600

    Solve problem 1360B Honest Coach!

---
+++ HonestCoach/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1360B Honest Coach!\n");
+
+
+int t = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i < t; i++)
+{
+    int n = int.Parse(Console.ReadLine()!);
+    int[] arr = new int[n];
+    string[] inputs = Console.ReadLine()!.Split();
+
+    for (int j = 0; j < n; j++)
+    {
+        arr[j] = int.Parse(inputs[j]);
+    }
+
+    Array.Sort(arr);
+
+    int result = arr[arr.Length - 1] - arr[0];
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int l = k + 1; l < n; l++)
+        {
+            result = Math.Min(result, (arr[l] - arr[k]));
+        }
+    }
+
+    Console.WriteLine(result);
+
+}
